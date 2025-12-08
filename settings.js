b3e059d57ff1e0033f0b6a804dcf736d331835e7
mwmndkhtrnaksltan-code <mwmndkhtrnaksltan@gmail.com>
2025-12-08 10:12:58 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7BgizYRJ#Gvm3rjK2X8_JWAeb4Z9adErLWOvZ8wopb6ewFLvrEas", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "eEp3WSQS#VTObKxHV_s7IZDkRHM5gwl4-L6zuxgQHWKq3kUpgk1s", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "967717147294",
